93b0cad2c29a39a781b587423e84de44b0c48ed5
KrzywdzinskiGrzegorz <krzywdzinskigrzegorz@gmail.com>
2022-12-11 00:02:35 +0100

    Add EntityFramework

---
+++ Diary/ViewModels/AddEditStudentViewModel.cs
@@ -0,0 +1,134 @@
+﻿using Diary.Commands;
+using Diary.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Diary.ViewModels
+{
+    public class AddEditStudentViewModel : ViewModelBase    
+    {
+        public AddEditStudentViewModel(Student student = null)
+        {
+            CloseCommand = new RelayCommand(Close);
+
+            ConfirmCommand = new RelayCommand(Confirm);
+
+            if(student == null) 
+            {
+                Student = new Student();
+            }
+            else
+            {
+                IsUpdate= true;
+                Student = student;
+            }
+
+            InitGroups();
+        }
+
+
+        public ICommand CloseCommand { get; set; }
+        public ICommand ConfirmCommand { get; set;}
+
+        private Student _student;
+
+        public Student Student
+        {
+            get { return _student; }
+            set 
+            { 
+                _student = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isUpdate;
+
+        public bool IsUpdate
+        {
+            get { return _isUpdate; }
+            set
+            {
+                _isUpdate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _selectedGroupId;
+
+        public int SelectedGroupId
+        {
+            get { return _selectedGroupId; }
+            set
+            {
+                _selectedGroupId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Group> _groups;
+
+        public ObservableCollection<Group> Groups
+        {
+            get { return _groups; }
+            set
+            {
+                _groups = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+
+        private void Confirm(object obj)
+        {
+            if (IsUpdate)
+                UpdateStudent();
+            else
+                AddStudent();
+
+            CloseWindow(obj as Window);
+        }
+
+        private void AddStudent()
+        {
+            //baza danych
+        }
+
+        private void UpdateStudent()
+        {
+            //baza danych
+        }
+
+        private void Close(object obj)
+        {
+            CloseWindow(obj as Window);
+        }
+
+        private void CloseWindow(Window window)
+        {
+            window.Close();
+        }
+
+        private void InitGroups()
+        {
+            Groups = new ObservableCollection<Group>
+            {
+                new Group{ Id= 0 , Name = "-- brak --"},
+                new Group{ Id= 1 , Name = "Klasa 1"},
+                new Group{ Id= 2 , Name = "Klasa 2"},
+                new Group{ Id= 3 , Name = "Klasa 3"},
+                new Group{ Id= 4 , Name = "Klasa 4"}
+            };
+
+            Student.Group.Id = 0;
+        }
+    }
+}
---
+++ Diary/ViewModels/MainViewModel.cs
@@ -0,0 +1,159 @@
+﻿using Diary.Commands;
+using Diary.Models;
+using Diary.Views;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Diary.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            RefreshStudentsCommand = new RelayCommand(RefreshStudents);
+            AddStudentCommand = new RelayCommand(AddEditStudent);
+            EditStudentCommand = new RelayCommand(AddEditStudent, CanEditDeleteStudent);
+            DeleteStudentCommand = new AsyncRelayCommand(DeleteStudent, CanEditDeleteStudent);
+
+            RefreshDiary();
+
+            InitGroups();
+        }
+
+
+        public ICommand RefreshStudentsCommand { get; set; }
+        public ICommand AddStudentCommand { get; set; }
+        public ICommand EditStudentCommand { get; set; }
+        public ICommand DeleteStudentCommand { get; set; }
+
+        private ObservableCollection<Student> _students;
+
+        public ObservableCollection<Student> Students
+        {
+            get { return _students; }
+            set
+            {
+                _students = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Student _selectedStudent;
+        public Student SelectedStudent 
+        {
+            get
+            {
+                return _selectedStudent;
+            }
+            set
+            {
+                _selectedStudent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Student Student
+        {
+            get { return _selectedStudent; }
+            set
+            {
+                _selectedStudent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _selectedGroupId;
+
+        public int SelectedGroupId
+        {
+            get { return _selectedGroupId; }
+            set
+            {
+                _selectedGroupId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Group> _groups;
+
+        public ObservableCollection<Group> Groups
+        {
+            get { return _groups; }
+            set
+            {
+                _groups = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        private void RefreshStudents(object obj)
+        {
+            RefreshDiary();
+        }
+
+        private async Task DeleteStudent(object obj)
+        {
+            var metroWindow = Application.Current.MainWindow as MetroWindow;
+            var dialog = await metroWindow.ShowMessageAsync(
+                "Usuwanie ucznia",
+                $"Czy napewno chcesz usunąć ucznia {SelectedStudent.FirstName} {SelectedStudent.LastName}?",
+                MessageDialogStyle.AffirmativeAndNegative);
+
+            if (dialog != MessageDialogResult.Affirmative)
+                return;
+            //usuwnaie ucznia z bazy danych
+
+            RefreshDiary(); 
+        }
+
+        private void AddEditStudent(object obj)
+        {
+            var addEditStudentView = new AddEditStudentView(obj as Student);
+            addEditStudentView.Closed += AddEditStudentView_Closed;
+            addEditStudentView.ShowDialog();
+        }
+
+        private void AddEditStudentView_Closed(object sender, EventArgs e)
+        {
+            RefreshDiary();
+        }
+
+        private bool CanEditDeleteStudent(object obj)
+        {
+            return SelectedStudent != null;
+        }
+
+        private void RefreshDiary()
+        {
+            Students = new ObservableCollection<Student>
+            {
+                new Student{FirstName = "Kazimierz", LastName="Kowalski", Group = new Group{ Id= 1} },
+                new Student{FirstName = "Marta", LastName="Nowak", Group = new Group{ Id= 2} },
+                new Student{FirstName = "Zuzanna", LastName="Kmieć", Group = new Group{ Id= 2} },
+                new Student{FirstName = "Adam", LastName="Paproch", Group = new Group{ Id= 1} }
+            };
+        }
+
+        private void InitGroups()
+        {
+            Groups = new ObservableCollection<Group>
+            {
+                new Group{ Id= 0 , Name = "Wszystkie"},
+                new Group{ Id= 1 , Name = "Klasa 1"},
+                new Group{ Id= 2 , Name = "Klasa 2"},
+                new Group{ Id= 3 , Name = "Klasa 3"},
+                new Group{ Id= 4 , Name = "Klasa 4"}
+            };
+
+            SelectedGroupId = 0;
+        }
+
+    }
+}
